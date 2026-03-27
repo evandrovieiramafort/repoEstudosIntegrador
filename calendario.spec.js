210823d916ecd5077dce635eef9569e9c95f3dc7
Evandro Vieira Mafort <81198982+evandrovieiramafort@users.noreply.github.com>
2026-03-26 21:47:43 -0300

    Update calendario.spec.js

--- calendario.spec.js
+++ calendario.spec.js
@@ -1,17 +1,20 @@
 import {expect, test} from "@playwright/test"
 
 test.describe("cefet-rj.br", () => {
-    test("apresenta opção de calendario", async ( {page} )=> {
+
+    test.beforeEach(async ({page}) => {
         await page.goto("https://cefet-rj.br")
-        const elemento = await page.getByText('Calendário')
-        expect(elemento).toBeDefined()
+    })
+
+    test("apresenta opção de calendario", async ( {page} )=> {
+        const elemento = await page.getByText('Calendários')
+        await expect(elemento).toBeVisible()
     })
 
     test("permite pesquisar um calendario", async ( {page} )=> {
-        await page.goto("https://cefet-rj.br")
         await page.fill("#portal-searchbox-field", "calendario nova friburgo")
-        await page.click('input[type="submit"]')
-        const elemento = await page.getByText('Calendário')
-        await expect(page.getByText("Nova Friburgo").toBeVisible())
+        await page.click('button[type="submit"]')
+        const elemento = await page.getByText('resultados encontrados')
+        await expect(elemento).toBeVisible()
     })
 })
